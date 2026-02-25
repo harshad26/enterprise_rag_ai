3e88780eab4cf7bc0288b58b1f290894d207cf3e
Apple <apple@Apples-MacBook-Pro.local>
2026-02-25 11:11:55 -0800

    changes in index file to store embedding into vectorstore

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
+from dotenv import load_dotenv
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+
+load_dotenv()
 
 pdf_path = Path(__file__).parent / "profile.pdf"
 
@@ -14,4 +19,18 @@ tetx_spitter = RecursiveCharacterTextSplitter(
   chunk_overlap = 400
 )
 
-tetx_spitter.split_documents(documents=docs)
+chunks = tetx_spitter.split_documents(documents=docs)
+
+# Vector Embedding
+embedding_model = OpenAIEmbeddings(
+  model = "text-embedding-3-large"
+)
+
+vectore_store = QdrantVectorStore.from_documents(
+  documents=chunks,
+  embedding=embedding_model,
+  url="http://localhost:6333",
+  collection_name="enterprise_rag"
+)
+
+print("This is done")
